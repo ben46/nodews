6a6326a17e9cb5d83f1e10c09ecd263e94a5caa7
Zhuoqian Zhou <zhuoqian.zhou@dalveytech.com>
2023-11-01 09:40:51 +0800

    modified:   package.json 	modified:   simpleserver.js 	new file:   yarn.lock

--- simpleserver.js
+++ simpleserver.js
@@ -1,20 +1,52 @@
-const { WebSocketServer,WebSocket } = require("ws")
+const http = require('http');
+const server = http.createServer();
+const io = require('socket.io')(server);
+const redis = require('socket.io-redis');
+const redisClient = require('ioredis')(); // Redis 客户端
 
-const wss = new WebSocketServer({ port: 4000 });
-console.log('start server at 4000');
+// 配置 Redis 适配器
+io.adapter(redis({ host: 'localhost', port: 6379 }));
 
-wss.on('connection', function connection(ws) {
-    console.log("one client connected!")
-    ws.on('message', function message(data, isBinary) {
+// 第一个频道：转发广播所有消息
+io.on('connection', (socket) => {
+    socket.on('message', (message) => {
+        // 广播消息到所有连接的客户端
+        io.emit('broadcast_message', message);
+    });
 
-        var _t1 = (new Date()).getTime();
-        console.log(_t1);
-        console.log(data.toString('utf8'))
+    socket.on('special_message', (message) => {
+        // 存储消息到 Redis（这里存储到名为 "special_messages" 的 Redis 列表）
+        redisClient.lpush('special_messages', message, (err, reply) => {
+            if (err) {
+                console.error('保存特定消息到 Redis 失败：', err);
+            }
+        });
+    });
 
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data, { binary: isBinary });
+    // 查询 Redis 中的特定消息
+    socket.on('query_special_messages', () => {
+        redisClient.lrange('special_messages', 0, -1, (err, messages) => {
+            if (err) {
+                console.error('查询特定消息失败：', err);
+            } else {
+                // 将消息发送给客户端
+                socket.emit('special_messages', messages);
+            }
+        });
+    });
+
+    // 消费 Redis 中的特定消息
+    socket.on('consume_special_messages', () => {
+        redisClient.lpop('special_messages', (err, message) => {
+            if (err) {
+                console.error('消费特定消息失败：', err);
+            } else if (message) {
+                // 发送消息给客户端
+                socket.emit('consumed_message', message);
             }
         });
     });
 });
+
+server.listen(3000);
+console.log("server started");
